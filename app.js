893cbd50ac230c33f5b60bb9de9603f3ba8494f4
Luis Pardo <luispardosuarez@gmail.com>
2023-11-08 22:14:51 +0100

    Modified the JavaScript to print only the selected values in multiple choice questions, after encountering several printing issues as it was initially printing all values

---
+++ app.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let personalData = document.getElementById('personalData');
+
+    personalData.addEventListener("submit", function(event) {
+        event.preventDefault(); 
+        readFormFields();
+    });
+    
+    function readFormFields() {
+        let personalData = document.getElementById('personalData');
+        let result = '';
+
+        for (let i = 0; i < personalData.elements.length; i++) {
+            let element = personalData.elements[i];
+
+            if (element.type === 'radio' || element.type === 'checkbox') {
+                if (element.checked) {
+                    result += element.value + '\n';
+                }
+            }
+
+            if (element.type === 'select-one') {
+                result += element.options[element.selectedIndex].value + '\n';
+            }
+
+            if (element.type === 'select-multiple') {
+                for (let j = 0; j < element.options.length; j++) {
+                    if (element.options[j].selected) {
+                        result += element.options[j].value + '\n';
+                    }
+                }
+            }
+
+            if (element.type !== 'radio' && element.type !== 'checkbox' && element.type !== 'select-one' && element.type !== 'select-multiple') {
+                result += element.value + '\n';
+            }
+        }
+
+        console.log(result);
+
+        return false;
+    }
+});
